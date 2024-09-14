9ffd49390e73394520e6db711b8b4143a6f52567
Daniel Roe <daniel@roe.dev>
2024-09-14 13:03:02 +0100

    chore: add nuxt fonts module

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hello ViteConf</h1>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  color: red;
+  font-family: Poppins, Arial;
+}
+</style>
